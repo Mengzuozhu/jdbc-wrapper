89e15b925fcb4ecbc79a1a6d157d139e02ccd664
Mengzuozhu <mengzuozhu@foxmail.com>
2022-02-08 21:28:29 +0800

    add comment

--- jdbc-wrapper-example/src/main/java/com/github/mengzz/jdbc/wrapper/example/config/CustomWhereInterceptor.java
+++ jdbc-wrapper-example/src/main/java/com/github/mengzz/jdbc/wrapper/example/config/CustomWhereInterceptor.java
@@ -4,8 +4,10 @@ import com.github.mengzz.jdbc.wrapper.interceptor.WhereInterceptor;
 import org.springframework.data.relational.core.sql.Where;
 
 /**
+ * 自定义拦截器，统一设置租户或公共字段等条件
+ *
  * @author mengzz
- **/
+ */
 public class CustomWhereInterceptor extends WhereInterceptor {
     private static Where record;
 
